50346ef7e73c0d91a419ca5943f240ce787da585
Wilson Yan <wilson1.yan@berkeley.edu>
2019-11-07 22:13:03 -0800

    Update to wilson paths

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
 import argparse
-from model import *
 from trainer import Trainer
-
+from model import *
 parser = argparse.ArgumentParser()
 
 # Path configurations
@@ -16,15 +15,15 @@ parser.add_argument("-prefix",
                     help="experiment prefix")
 parser.add_argument("-fcnpath",
                     type=str,
-                    default='/home/thanard/Downloads/FCN_mse',
+                    default='/home/wilson/causal-infogan/data/FCN_mse',
                     help="path to fcn parameters for background subtraction")
 parser.add_argument("-data_dir",
                     type=str,
-                    default='/home/thanard/Downloads/rope_full',
+                    default='/home/wilson/causal-infogan/data/rope_full',
                     help='path to rope_full data')
 parser.add_argument("-planning_data_dir",
                     type=str,
-                    default='/home/thanard/Downloads/seq_data_2',
+                    default='/home/wilson/causal-infogan/data/seq_data_2',
                     help='path to seq_data_2 data')
 parser.add_argument("-loadpath",
                     type=str,
