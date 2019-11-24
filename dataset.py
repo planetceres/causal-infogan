a15c9ad5c308bebba577b00ee7b9e720b911d7e4
Wilson Yan <wilson1.yan@berkeley.edu>
2019-11-23 21:38:47 -0800

    CPC script

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import torch.utils.data as data
+import torch
 import numpy as np
 import pickle as pkl
 import os
@@ -95,6 +96,54 @@ def make_pair(imgs, resets, k, get_img, root):
             next_img = imgs[i + k]
             image_pairs.append(((img[0], np.array(1.0, dtype='float32')),
                                 (next_img[0], np.array(1.0, dtype='float32'))))
+        pbar.update(1)
+    pbar.close()
+
+    with open(filename, 'wb') as f:
+        pkl.dump(image_pairs, f)
+    return image_pairs
+
+
+def make_cpc(imgs, resets, k, n, n_repeat, get_img, root):
+    """
+    Return a list of image pairs. The pair is picked if they are k steps apart,
+    and there is no reset from the first to the k-1 frames.
+    Cases:
+        If k = -1, we just randomly pick two images.
+        If k >= 0, we try to load img pairs that are k frames apart.
+    """
+    if k < 0:
+        return list(zip(imgs, np.random.permutation(imgs)))
+
+    filename = os.path.join(root, 'rope_pairs_%d.pkl' % k)
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            return pkl.load(f)
+
+    idxs = np.argwhere(resets).reshape(-1)
+    idxs = np.concatenate(([-1], idxs), axis=0)
+    episodes = []
+    for i in range(len(idxs) - 1):
+        episodes.append((idxs[i] + 1, idxs[i + 1] + 1))  # (inclusive, exclusive)
+
+    image_pairs = []
+    pbar = tqdm(total=len(imgs))
+    for i, img in enumerate(imgs):
+        if np.sum(resets[i:i + k]) == 0 and (get_img(imgs[i + k][0]) - get_img(img[0])).abs().max() > 0.5:
+            # Get True next image
+            true_next_img = imgs[i + k]
+
+            # Get negative samples
+            ep_idxs = np.random.randint(0, len(episodes), size=n_repeat * (n - 1))
+            while (episodes[ep_idxs][0] <= i < episodes[ep_idxs][1]).any():
+                ep_idxs = np.random.randint(0, len(episodes), size=n_repeat * (n - 1))
+            false_next_imgs = [np.random.randint(episodes[ep_idx][0], episodes[ep_idx][1]) for ep_idx in ep_idxs]
+            false_next_imgs = [imgs[next_img] for next_img in false_next_imgs]
+
+            labels = np.random.randint(low=0, high=n, size=n_repeat).tolist()
+            for i in range(n_repeat):
+                label = labels[i]
+                image_pairs.append(([img] + false_next_imgs[:label] + [true_next_img] + false_next_imgs[label:], label))
         pbar.update(1)
     pbar.close()
 
@@ -258,3 +307,84 @@ class ImagePairs(data.Dataset):
         tmp = '    Target Transforms (if any): '
         fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
         return fmt_str
+
+    class CPCDataset(data.Dataset):
+        """
+        A copy of ImageFolder from torchvision. Output image pairs that are k steps apart.
+
+        Args:
+            root (string): Root directory path.
+            transform (callable, optional): A function/transform that  takes in an PIL image
+                and returns a transformed version. E.g, ``transforms.RandomCrop``
+            target_transform (callable, optional): A function/transform that takes in the
+                target and transforms it.
+            loader (callable, optional): A function to load an image given its path.
+            n_frames_apart (int): The number of frames between the image pairs. Fixed for now.
+
+         Attributes:
+            classes (list): List of the class names.
+            class_to_idx (dict): Dict with items (class_name, class_index).
+            img_pairs (list): List of pairs of (image path, class_index) tuples
+        """
+
+        def __init__(self, root, transform=None, target_transform=None,
+                     loader=default_loader, n_frames_apart=1, n=5, n_repeat=5):
+            self.root = root
+
+            classes, class_to_idx = find_classes(root)
+            imgs, actions = make_dataset(root, class_to_idx)
+            if len(imgs) == 0:
+                raise (RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                                                                                 "Supported image extensions are: " + ",".join(
+                    IMG_EXTENSIONS)))
+            resets = 1. - actions[:, -1]
+            assert len(imgs) == len(resets)
+
+            self.classes = classes
+            self.class_to_idx = class_to_idx
+            self.transform = transform
+            self.target_transform = target_transform
+            self.loader = loader
+            img_pairs = make_cpc(imgs, resets, n_frames_apart, n, n_repeat, self._get_image, root)
+            self.img_pairs = img_pairs
+
+        def _get_image(self, path):
+            img = self.loader(path)
+            if self.transform is not None:
+                img = self.transform(img)
+            return img
+
+        def _check_exists(self):
+            return os.path.exists(self.processed_folder)
+
+        @property
+        def raw_folder(self):
+            return os.path.join(self.root, self.__class__.__name__, 'raw')
+
+        @property
+        def processed_folder(self):
+            return os.path.join(self.root, self.__class__.__name__, 'processed')
+
+        def __getitem__(self, index):
+            """
+            Args:
+                index (int): Index
+
+            Returns:
+                tuple: (image, target) where target is class_index of the target class.
+            """
+            paths, target = self.img_pairs[index]
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+
+            images = []
+            for path in paths:
+                img = self.loader(path)
+                if self.transform is not None:
+                    img = self.transform(img)
+                images.append(img)
+            images = torch.stack(images, dim=0)
+            return images, target
+
+        def __len__(self):
+            return len(self.img_pairs)
---
+++ train_cpc.py
@@ -0,0 +1,197 @@
+import argparse
+import os
+from os.path import join, exists
+import numpy as np
+from scipy.ndimage.morphology import grey_dilation
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+
+from torchvision.util import save_image
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+from dataset import CPCDataset
+
+
+class Encoder(nn.Module):
+    def __init__(self, z_dim, channel_dim):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(channel_dim, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 64 x 32 x 32
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 128 x 16 x 16
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # Option 1: 256 x 8 x 8
+            nn.Conv2d(256, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # 256 x 4 x 4
+        )
+        self.out = nn.Linear(256 * 4 * 4, z_dim)
+
+    def forward(self, x):
+        x = self.model(x)
+        return self.out(x)
+
+
+class Transition(nn.Module):
+    def __init__(self, z_dim):
+        self.out = nn.Linear(z_dim, z_dim, bias=False)
+
+    def forward(self, x):
+        return self.out(x)
+
+
+def infinite_dataloader(dataloader):
+    while True:
+        for batch in dataloader:
+            yield batch
+
+
+def get_dataloader():
+    def filter_background(x):
+        x[:, (x < 0.3).any(dim=0)] = 0.0
+        return x
+
+    def dilate(x):
+        x = x.squeeze(0).numpy()
+        x = grey_dilation(x, size=3)
+        x = x[None, :, :]
+        return torch.from_numpy(x)
+
+    transform = transforms.Compose([
+        transforms.Resize(64),
+        transforms.CenterCrop(64),
+        transforms.ToTensor(),
+        filter_background,
+        lambda x: x.mean(dim=0)[None, :, :],
+        dilate,
+        transforms.Normalize((0.5,), (0.5,)),
+    ])
+
+    train_dset = CPCDataset(root=args.train_root, transform=transform,
+                            n_frames_apart=args.k, n=args.n, n_repeat=args.n_repeat)
+    train_loader = data.DataLoader(train_dset, batch_size=args.bs,
+                                   shuffle=True, num_workers=2)
+
+    other_dset = ImageFolder(args.root, transform=transform)
+    other_loader = data.DataLoader(other_dset, batch_size=args.batch_size, shuffle=True,
+                                   pin_memory=True, num_workers=2)
+
+    start_dset = ImageFolder(args.start, transform=transform)
+    goal_dset = ImageFolder(args.goal, transform=transform)
+
+    start_images = torch.stack([start_dset[i] for i in range(len(start_dset))], dim=0)
+    goal_images = torch.stack([goal_dset[i] for i in range(len(goal_dset))], dim=0)
+
+
+    return infinite_dataloader(train_loader), other_loader, start_images, goal_images
+
+
+def find_closest_image(z, other_loader, encoder):
+    with torch.no_grad():
+        dists = []
+        for other_x, _ in other_loader:
+            other_x = other_x.cuda()
+            other_z = encoder(other_x) # bs x z_dim
+            dists.append(torch.matmul(z, other_z.t()).cpu())
+        dists = torch.cat(dists, dim=1)
+        closest = torch.argmin(dists).numpy()
+
+        closest_images = torch.stack([other_loader.dataset[i] for i in closest], dim=0)
+        return closest_images * 0.5 + 0.5
+
+
+
+def main():
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+    folder_name = join('out', args.name)
+    if not exists(folder_name):
+        os.makedirs(folder_name)
+
+    obs_dim = (1, 64, 64)
+    encoder = Encoder(args.z_dim, 1).cuda()
+    trans = Transition(args.z_dim).cuda()
+
+    optimizer = optim.Adam(list(encoder.parameters()) + list(trans.parameters()),
+                           lr=args.lr)
+
+    dataloader, other_loader, start_images, goal_images = get_dataloader()
+    start_images, goal_images = start_images.cuda(), goal_images.cuda()
+    losses = []
+
+    x, _ = next(dataloader)
+    x = x.view(-1, *obs_dim)
+    save_image(x * 0.5 + 0.5, join(folder_name, 'train_img.png'))
+
+    pbar = tqdm(total=args.itrs)
+    for i in range(args.itrs):
+        x, y = next(dataloader) # bs x n+1 x 1 x 64 x 64
+        x, y = x.cuda(), y.cuda()
+
+        x = x.view(-1, *obs_dim) # bs * (n + 1) x 1 x 64 x 64
+        z = encoder(x) # bs * (n + 1) x z_dim
+        z = z.view(args.bs, args.n + 1, *obs_dim)  # bs x n+1 x z_dim
+        z_cur, z_other = z[:, 0, :], z[:, 1:, :] # bs x 1 x z_dim, bs x n x z_dim
+        z_next = trans(z_cur) # bs x 1 x z_dim
+        z_other = z_other.permute(0, 2, 1).contiguous() # bs x z_dim x n
+        logits = torch.bmm(z_next, z_other).squeeze(1) # bs x n
+        loss = F.cross_entropy(logits, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(losses.item())
+        avg_loss = np.mean(losses[-50:])
+        pbar.set_description('Loss: {:.4f}'.format(avg_loss))
+        pbar.update(1)
+
+        if i % args.log_interval:
+            z_start = encoder(start_images) # n x z_dim
+            z_goal = encoder(goal_images) # n x z_dim
+
+            lambdas = np.linspace(0, 1, args.n_interp + 2)
+            zs = torch.stack([(1 - lambda_) * z_start + lambda_ * z_goal
+                              for lambda_ in lambdas], dim=1) # n x n_interp+2 x z_dim
+            zs = zs.view(-1, args.z_dim) # n * (n_interp+2) x z_dim
+            imgs = find_closest_image(zs, other_loader, encoder)
+            save_image(imgs, join(folder_name, 'sample_itr{}.png'.format(i)), nrow=args.n_interp + 2)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type=str, default='data/rope/full_data')
+    parser.add_argument('--start', type=str, default='data/rope/seq_data/start')
+    parser.add_argument('--goal', type=str, default='data/rope/seq_data/goal')
+    parser.add_argument('--n_interp', type=int, default=6)
+
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--lr', type=float, default=2e-4)
+    parser.add_argument('--itrs', type=int, default=int(1e5))
+    parser.add_argument('--log_interval', type=int, default=1000)
+
+    parser.add_argument('--n', type=int, default=5)
+    parser.add_argument('--n_repeat', type=int, default=5)
+    parser.add_argument('--z_dim', type=int, default=16)
+    parser.add_argument('--k', type=int, default=1)
+
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--name', type=str, default='cpc')
+    args = parser.parse_args()
+    main()
