41fa38151ca2ffa1f636572ea6019bf234bcd9b0
Wilson Yan <wilson1.yan@berkeley.edu>
2019-12-05 21:51:00 -0800

    added collect all imgs

--- compute_vine_dset.py
+++ compute_vine_dset.py
@@ -5,6 +5,7 @@ import os
 from os.path import join
 import numpy as np
 import itertools
+from tqdm import tqdm
 
 
 def org_images(images):
@@ -14,7 +15,7 @@ def org_images(images):
         t = int(img_split[-2])
         k = int(img_split[-1].split('.')[0])
         if t not in t_k:
-            t_k[t][k] = dict()
+            t_k[t] = dict()
         assert k not in t_k[t]
         t_k[t][k] = image
     return t_k
@@ -30,7 +31,8 @@ runs = sorted(runs)
 neg_samples_same_t = dict()
 neg_samples_same_traj = dict()
 pos_pairs = []
-for run in runs:
+all_images = []
+for run in tqdm(runs):
     action_file = join(run, 'actions.npy')
     np.load(action_file) # just to make sure the path is right
 
@@ -39,6 +41,7 @@ for run in runs:
 
     images = glob.glob(join(run, '*.png'))
     images = sorted(images)
+    all_images.extend(images)
     images = org_images(images)
     for t in itertools.count():
         if t + 1 not in images:
@@ -55,6 +58,6 @@ for run in runs:
                 neg_samples_same_traj[run][t].append(images[t_tmp][k_tmp])
 
 data = dict(pos_pairs=pos_pairs, neg_samples_t=neg_samples_same_t,
-            neg_samples_traj=neg_samples_same_traj)
+            neg_samples_traj=neg_samples_same_traj, all_images=all_images)
 with open(join(args.root, 'pos_neg_pairs.pkl'), 'wb') as f:
     pickle.dump(data, f)
